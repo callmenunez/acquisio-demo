0d1df6becae09b2028fcdcf96f01e3cf32b02f38
David Nunez <david.nunez@yodle.com>
2019-01-06 22:19:32 -0500

    undid all changes to index file since local heroku app was not launching with my last changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 
-// const awsSave = require('./middleware/aws-save').middlewareStack;
+const awsSave = require('./middleware/aws-save').middlewareStack;
 const awsRetrieve = require('./middleware/aws-retrieve').middlewareStack;
 
 const app = express();
@@ -16,24 +16,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/:customCode', awsRetrieve, function(req, res, next) {
+
   function logResponseBody(req, res, next) {
     var oldWrite = res.write,
         oldEnd = res.end;
+  
     var chunks = [];
+  
     res.write = function (chunk) {
       chunks.push(chunk);
+  
       oldWrite.apply(res, arguments);
     };
+  
     res.end = function (chunk) {
       if (chunk)
         chunks.push(chunk);
+
       var body = Buffer.concat(chunks).toString('utf8');
       console.log(req.path, body);
+  
       oldEnd.apply(res, arguments);
     };
+  
     next();
   }
+  
   app.use(logResponseBody);
 
   const brandDomain = req.headers.host;
@@ -59,26 +70,22 @@ app.use('/:customCode', awsRetrieve, function(req, res, next) {
   // }
 });
 
-app.use('/', function(req, res, next) {
+
+app.use('/', function(req, res) {
+  // app.use('/', function(req, res, next) {
     const brandDomain = req.headers.host;
   
-    if (brandDomain.indexOf('demo.acquisio') >= 0) {
+    // if (brandDomain.indexOf('demo.acquisio') >= 0) {
       res.render('views/acquisio-demo/index.html', {
         // className: 'home',
         metaDescription: '',
         title: 'Acquisio Demo test'
       });
-    }
+    // }
   });
 
 // start app ===============================================
 app.listen(port);
-
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-// app.listen(port);
 // shoutout to the user
 console.log(`Server running on port ${port}`);
 
